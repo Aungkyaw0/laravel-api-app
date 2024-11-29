a3c38615c6643bbfae3db6416e7c019269a771ca
Aungkyaw0 <a.k.t64690@gmail.com>
2024-11-30 02:30:27 +0630

    "Route setups for all register types"

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,18 @@ MemberController::class);
 Route::post('/registers/member',
  [AuthController::class, 'registerMember']);
 
+ #This route is for caregiver registeration
+Route::post('/registers/caregiver',
+[AuthController::class, 'registerCaregiver']);
+
+#This route is for partner registeration
+Route::post('/registers/partner',
+ [AuthController::class, 'registerPartner']);
+
+#This route is for volunteer registeration
+Route::post('/registers/volunteer',
+ [AuthController::class, 'registerVolunteer']);
+
 #This route is for login
 Route::post('/login',
  [AuthController::class, 'login']);
